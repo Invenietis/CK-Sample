961b32590a9fee9e3648e3fa04e88ba31f5fad77
Pierre Viara <pierre.viara@signature-code.com>
2018-06-06 12:57:58 +0200

    Updated Startup.cs to handle OAuth Authentication (GitHub).

--- CKS/WebApp/Startup.cs
+++ CKS/WebApp/Startup.cs
@@ -1,16 +1,25 @@
 using System;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CK.AspNet.Auth;
+using Newtonsoft.Json.Linq;
 using CK.Auth;
+using CK.AspNet.Auth;
 using CK.DB.AspNet.Auth;
+using CK.DB.User.UserGithub;
 
 namespace WebApp
 {
-    class Startup
+    public class Startup
     {
         readonly IConfiguration _configuration;
         readonly IHostingEnvironment _env;
@@ -29,7 +38,40 @@ namespace WebApp
                  {
                      options.ExpireTimeSpan = TimeSpan.FromHours( 1 );
                      options.SlidingExpirationTime = TimeSpan.FromHours( 1 );
-                 } );
+                 } )
+                 .AddOAuth( "GitHub", options =>
+                  {
+                      options.ClientId = _configuration["Authentication:GitHub:ClientId"];
+                      options.ClientSecret = _configuration["Authentication:GitHub:ClientSecret"];
+                      options.CallbackPath = new PathString( "/signin-github" );
+
+                      options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+                      options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+                      options.UserInformationEndpoint = "https://api.github.com/user";
+
+                      options.ClaimActions.MapJsonKey( ClaimTypes.NameIdentifier, "id" );
+                      options.ClaimActions.MapJsonKey( ClaimTypes.Name, "name" );
+                      options.ClaimActions.MapJsonKey( "urn:github:login", "login" );
+                      options.ClaimActions.MapJsonKey( "urn:github:url", "html_url" );
+                      options.ClaimActions.MapJsonKey( "urn:github:avatar", "avatar_url" );
+
+                      options.Events = new OAuthEventHandler
+                      {
+                          OnCreatingTicket = async context =>
+                          {
+                              var request = new HttpRequestMessage( HttpMethod.Get, context.Options.UserInformationEndpoint );
+                              request.Headers.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
+                              request.Headers.Authorization = new AuthenticationHeaderValue( "Bearer", context.AccessToken );
+
+                              var response = await context.Backchannel.SendAsync( request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted );
+                              response.EnsureSuccessStatusCode();
+
+                              var user = JObject.Parse( await response.Content.ReadAsStringAsync() );
+
+                              context.RunClaimActions( user );
+                          }
+                      };
+                  } );
 
             if( _env.IsDevelopment() )
             {
@@ -53,6 +95,17 @@ namespace WebApp
 
             app.UseAuthentication();
         }
+    }
 
+    internal class OAuthEventHandler : OAuthEvents
+    {
+        public override Task TicketReceived( TicketReceivedContext c )
+        {
+            var authService = c.HttpContext.RequestServices.GetRequiredService<WebFrontAuthService>();
+            return authService.HandleRemoteAuthentication<IUserGithubInfo>( c, payload =>
+            {
+                payload.GithubAccountId = c.Principal.FindFirst( ClaimTypes.NameIdentifier ).Value;
+            } );
+        }
     }
 }
